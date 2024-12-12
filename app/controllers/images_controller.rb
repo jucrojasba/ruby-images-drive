c460a5dfa90087d8af9df67e6f22e2f1d393fc97
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-12-12 07:22:02 -0500

    Add views

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,13 +1,30 @@
 class ImagesController < ApplicationController
   def index
+    @images = Image.all
   end
 
   def new
+    @image = Image.new
   end
 
   def create
+    @image = Image.new(image_params)
+    if @image.save
+      flash[:success] = I18n.t('flash.success')
+      redirect_to @image
+    else
+      flash.now[:error] = I18n.t('flash.error')
+      render :new
+    end
   end
 
   def show
+    @image = Image.find(params[:id])
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:title, :description, :file)
   end
 end
