4f862d439d0ef539cbb11c1f21572888a4e8e37d
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-12-12 08:30:36 -0500

    Fix bug validator

--- app/models/image.rb
+++ app/models/image.rb
@@ -11,12 +11,14 @@ class Image < ApplicationRecord
   def file_validation
     return unless file.attached?
 
+    # Validación de tipo de archivo
     if !file.content_type.in?(%w[image/png image/jpg image/jpeg])
-      errors.add(:file, :invalid_format)
+      errors.add(:file, I18n.t('errors.messages.invalid_file_type'))
     end
 
+    # Validación de tamaño de archivo
     if file.blob.byte_size > 3.megabytes
-      errors.add(:file, :too_large)
+      errors.add(:file, I18n.t('errors.messages.file_too_large'))
     end
   end
 end
