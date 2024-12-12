6756adc5383777a401649dbf96c69f6b839e56a7
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-12-12 08:16:18 -0500

    Modify error handle, add styles to all views

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,22 @@
 class Image < ApplicationRecord
   has_one_attached :file
-  validates :file, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
-                   size: { less_than: 3.megabytes }
+
+  validate :file_validation
+
   validates :title, length: { maximum: 100 }
   validates :description, length: { maximum: 500 }
+
+  private
+
+  def file_validation
+    return unless file.attached?
+
+    if !file.content_type.in?(%w[image/png image/jpg image/jpeg])
+      errors.add(:file, :invalid_format)
+    end
+
+    if file.blob.byte_size > 3.megabytes
+      errors.add(:file, :too_large)
+    end
+  end
 end
